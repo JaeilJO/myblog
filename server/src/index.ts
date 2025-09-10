0c1c161eaef2270f3effeaec8a620f299bec05d9
jaeiljo <jaeiljo@jaeiljoui-MacBookPro.local>
2025-09-10 20:21:08 +0900

    Create Login API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import authRouter from './routes/auth';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (_req, res) => {
   res.send('Hello World');
 });
+
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
